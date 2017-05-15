80dee019245f7c0b764fbac06b9f7591b3ae1290
Stormican <fenderbender004@gmail.com>
2017-05-15 13:49:57 -0600

    Start working on the bug fix for null pointer

--- src/GitRunner.java
+++ src/GitRunner.java
@@ -3,6 +3,7 @@ public class GitRunner {
 
 	public static void main(String[] args) {
 		System.out.println("Initial Build - new project, yeah!");
+		System.out.println("Fix null pointer bug");
 
 	}
 
